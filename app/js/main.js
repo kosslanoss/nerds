eb8b791a60cd9cb526593114fab7ed834a328882
kosslanoss <kosslanoss@gmail.com>
2021-11-24 22:42:07 +0200

    fixed about__counter

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,18 @@ $(function(){
     fade: true
   });
 
+  
+  $('.about__counter').each(function () {
+    $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+    }, {
+      duration: 3000,
+      easing: 'swing',
+      step:function (now){
+        $(this).text(Math.ceil(now));
+      }
+    });
+  });
 
 
-
-});
+})
