5c8112145ee9f2e5ce920f0d565bf33b8e3f73eb
kosslanoss <kosslanoss@gmail.com>
2021-12-23 20:19:02 +0200

    fixed product & popup

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,44 @@
 $(function(){
 
+  let popup = document.querySelector(".popup");
+  let popupForm = document.querySelector(".popup__form");
+  let popupBtn = document.querySelector(".map__btn");
+  let popupClose = document.querySelector(".popup__btn-close");
+
+  popupBtn.addEventListener("click", (e) => {
+    e.preventDefault;
+     showPopup();
+  });
+
+  popupClose.addEventListener("click", (e) => {
+     e.preventDefault;
+     removePopup();
+   });
+
+  popupForm.addEventListener("submit", () => {
+     removePopup();
+  });
+
+  popup.addEventListener("click", (e) => {
+    let target = e.target;
+    if (target.classList.contains("popup")) {
+      removePopup();
+    } else return;
+  });
+
+  function showPopup() {
+    popup.classList.add("active");
+    bodyScroll();
+  }
+
+  function removePopup() {
+    popup.classList.remove("active");
+    bodyScroll();
+   }
+
+  function bodyScroll() {
+    document.body.classList.toggle("no-scroll");
+  }
 
 
   $('.filter-price__input').ionRangeSlider({
