1f6ad7d20ed39cb50cc6a0f46e417e86295c8b24
kosslanoss <kosslanoss@gmail.com>
2022-03-22 22:07:49 +0200

    adaptive header footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,10 @@
 $(function(){
 
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active');
+    $('.menu__btn').toggleClass('menu__btn--active');
+  });
+
 $('.hero__slider').slick({
     dots: true,
     arrows: false,
